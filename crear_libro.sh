ec6f1f12f4bf58045d4db581613bb84bedf4241d
Gorka <gorkau@gmail.com>
2018-07-15 20:24:15 +0200

    Comienzo del libro. Capítulo 1

---
+++ crear_libro.sh
@@ -0,0 +1,9 @@
+# Software necesario
+# sudo apt install asciidoc
+# sudo apt install libxml2-utils
+# sudo apt install dblatex
+
+# AsciiDoctor PDF
+asciidoctor -r asciidoctor-pdf -b pdf book.adoc
+
+
